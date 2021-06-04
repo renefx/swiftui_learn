271933222f86b65fa260170528ef9323a426ee61
Rene X <renefx@gmail.com>
2021-06-03 23:45:28 -0300

    Card with shadow and glassmophic style

---
+++ SwiftUI Learn/Gradient/Glassmorphism II/Card/CardGlass.swift
@@ -0,0 +1,47 @@
+//
+//  CardGlass.swift
+//  SwiftUI Learn
+//
+//  Created by Rene Freire Xavier on 03/06/21.
+//
+
+import SwiftUI
+
+struct CardGlass: View {
+    var colors: [Color]
+    var gradientBackground: LinearGradient
+    
+    init(colors: [Color] = [.red, .clear]) {
+        self.colors = colors
+        gradientBackground = LinearGradient(
+                                gradient: .init(colors: colors),
+                                startPoint: .topLeading,
+                                endPoint: .bottomTrailing)
+    }
+    
+    var body: some View {
+        VStack() {
+            FormerElementsCard(bankName: "Superbank", cardNumber: "1234 5678 1234 5678")
+            
+            
+            LatterElementsCard(validDate: "12/28", holderName: "Rene x")
+                .clipped()
+                .background(gradientBackground)
+        }
+        .aspectRatio(1.5, contentMode: .fill)
+        .background(
+            Color.white.blur(radius: 30.0)
+        )
+        .cornerRadius(30)
+        .shadow(radius: 6, x: 10, y: 10)
+        .aspectRatio(1.5, contentMode: .fit)
+        .frame(maxWidth: UIScreen.main.bounds.size.width * 0.8,
+               maxHeight: UIScreen.main.bounds.size.width * 0.8)
+    }
+}
+
+struct CardGlass_Previews: PreviewProvider {
+    static var previews: some View {
+        CardGlass()
+    }
+}
