c2ee50f7265246ccb20698445f23d8acbeed3a59
Rene X <renefx@gmail.com>
2021-06-03 23:43:43 -0300

    Gradient circle for card

---
+++ SwiftUI Learn/Gradient/Glassmorphism II/GradientCircle.swift
@@ -0,0 +1,36 @@
+//
+//  GradientCircle.swift
+//  SwiftUI Learn
+//
+//  Created by Rene Freire Xavier on 03/06/21.
+//
+
+import SwiftUI
+
+struct GradientCircle: View {
+    var colors: [Color] = [.red, .blue]
+    var shadowPostion: (CGFloat, CGFloat) = (10,10)
+    var body: some View {
+        LinearGradient(gradient: .init(colors: colors),
+                                    startPoint: .topLeading,
+                                    endPoint: .bottomTrailing)
+            .aspectRatio(1, contentMode: .fit)
+            .clipShape(Circle())
+            .shadow(color: .gray, radius: 8, x: shadowPostion.0, y: shadowPostion.1)
+    }
+}
+
+extension View {
+    public func gradientForeground(colors: [Color]) -> some View {
+        self.overlay(LinearGradient(gradient: .init(colors: colors),
+                                    startPoint: .topLeading,
+                                    endPoint: .bottomTrailing))
+            .mask(self)
+    }
+}
+
+struct GradientCircle_Previews: PreviewProvider {
+    static var previews: some View {
+        GradientCircle()
+    }
+}
