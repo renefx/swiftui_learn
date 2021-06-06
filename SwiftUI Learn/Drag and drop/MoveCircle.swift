0fb085ec62ade161a2da94f456dd8df6cef2185e
Rene X <renefx@gmail.com>
2021-06-06 00:37:14 -0300

    Removes infinite loop in GeometryReader

---
+++ SwiftUI Learn/Drag and drop/MoveCircle.swift
@@ -0,0 +1,42 @@
+//
+//  MoveCircle.swift
+//  SwiftUI Learn
+//
+//  Created by Rene Freire Xavier on 05/06/21.
+//
+
+import SwiftUI
+
+struct GenericList: View {
+    @State var timesCalled = 0
+    
+    // Validation to only get the frame once
+    @ViewBuilder
+    func geometryReader() -> some View {
+        if timesCalled < 1 {
+            GeometryReader { geometry -> Color in
+                DispatchQueue.main.async {
+                    timesCalled += 1
+                }
+                return Color.clear
+            }
+        } else {
+            Color.black
+        }
+    }
+
+    var body: some View {
+        VStack() {
+            Text("\(timesCalled)")
+                .overlay(geometryReader())
+            Text("This view presents a solution to updating a @State inside a GeometryReader. It will only get it once.\nAny other method creates an infinite loop.")
+                .padding()
+        }
+    }
+}
+
+struct MoveCircle_Previews: PreviewProvider {
+    static var previews: some View {
+        GenericList()
+    }
+}
