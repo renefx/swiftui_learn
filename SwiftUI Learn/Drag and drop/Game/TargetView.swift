5c85c468a7841df1a9d042d7a1a3b7cf7b5923a4
Rene X <renefx@gmail.com>
2021-06-06 00:31:00 -0300

    Only gets the frame for the target once

--- SwiftUI Learn/Drag and drop/Game/TargetView.swift
+++ SwiftUI Learn/Drag and drop/Game/TargetView.swift
@@ -9,23 +9,32 @@ import SwiftUI
 
 struct TargetView: View {
     @ObservedObject var model: DragViewModel
+    @State private var calculatePosition = true
     
     private let circleColors = [
         Color.white.opacity(0.7),
         Color.white.opacity(0.01)]
     
+    @ViewBuilder
+    func geometryReader() -> some View {
+        if calculatePosition {
+            GeometryReader { geometry -> Color in
+                DispatchQueue.main.async {
+                    model.targetFrame = geometry.frame(in: .global) // in window
+                    calculatePosition = false
+                }
+                return Color.clear
+            }
+        } else {
+            EmptyView()
+        }
+    }
+    
     var body: some View {
         ZStack() {
             GradientCircle(colors: circleColors)
                 .frame(width: 90)
-                .overlay(
-                    GeometryReader { geometry -> Color in
-                        DispatchQueue.main.async {
-                            model.targetFrame = geometry.frame(in: .global) // in window
-                        }
-                        return Color.clear
-                    }
-                )
+                .overlay(geometryReader())
                 .overlay(
                     Text("HERE")
                         .fontWeight(.black)
