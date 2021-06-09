807ff23f2a0cc20011956c2826c65e6e77d3bc6b
Rene X <renefx@gmail.com>
2021-06-09 00:35:38 -0300

    Arc view created

---
+++ SwiftUI Learn/ArcView.swift
@@ -0,0 +1,20 @@
+//
+//  ArcView.swift
+//  SwiftUI Learn
+//
+//  Created by Rene Freire Xavier on 09/06/21.
+//
+
+import SwiftUI
+
+struct ArcView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ArcView_Previews: PreviewProvider {
+    static var previews: some View {
+        ArcView()
+    }
+}
