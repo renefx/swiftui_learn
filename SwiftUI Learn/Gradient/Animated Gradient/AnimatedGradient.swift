75b783ec3ec7c4a0d9019a43ed650b08da2f0f59
Rene X <renefx@gmail.com>
2021-06-10 01:32:18 -0300

    Folder organization

---
+++ SwiftUI Learn/Gradient/Animated Gradient/AnimatedGradient.swift
@@ -0,0 +1,21 @@
+//
+//  AnimatedGradient.swift
+//  SwiftUI Learn
+//
+//  Created by Rene Freire Xavier on 09/06/21.
+//
+
+import SwiftUI
+
+struct AnimatedGradient: View {
+    var body: some View {
+        
+        Text("Hello")
+    }
+}
+
+struct AnimatedGradient_Previews: PreviewProvider {
+    static var previews: some View {
+        AnimatedGradient()
+    }
+}
