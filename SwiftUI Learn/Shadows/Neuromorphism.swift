ec0248ae4f2fa03fc239528c5609ccfc1ac18c60
Rene X <renefx@gmail.com>
2021-06-17 23:12:11 -0300

    Strating ring view

--- SwiftUI Learn/Shadows/Neuromorphism.swift
+++ SwiftUI Learn/Shadows/Neuromorphism.swift
@@ -98,3 +98,46 @@ struct Neuromorphism_Previews: PreviewProvider {
         Neuromorphism()
     }
 }
+
+
+struct RingView: View {
+    var color1  = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
+    var color2  = #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)
+    var width: CGFloat = 300
+    var height: CGFloat = 300
+    var percent: CGFloat = 88
+    @Binding var show : Bool
+    
+    var body: some View {
+        let multiplier = width / 44
+        let progress = 1 - (percent / 100)
+        
+        return ZStack {
+//            Circle()
+//                .stroke(Color.black.opacity(0.1), style: StrokeStyle(lineWidth: 5 * multiplier))
+//                .frame(width: width, height: height)
+            
+            Circle()
+//                .fill(Color.white)
+//                .trim(from: progress, to: 1)
+                .stroke(
+                    LinearGradient(gradient: Gradient(colors: [Color(color1), Color(color2)]), startPoint: .topTrailing, endPoint: .bottomLeading),
+                    style: StrokeStyle(lineWidth: 5 * multiplier, lineCap: .round, lineJoin: .round, miterLimit: .infinity, dash: [20, 0], dashPhase: 0)
+                )
+                .rotationEffect(Angle(degrees: 90))
+                .rotation3DEffect(Angle(degrees: 180), axis: (x: 1, y: 0, z: 0))
+                .frame(width: width, height: height)
+                .shadow(color: Color(color2).opacity(0.1), radius: 3 * multiplier, x: 0, y: 3 * multiplier)
+                .shadow(color: Color(#colorLiteral(red: 0.1891615391, green: 0.03714629263, blue: 0.5007147193, alpha: 1)), radius: 6, x: 10, y: 10)
+                .shadow(color: Color(#colorLiteral(red: 0.6106063128, green: 0.5005851388, blue: 0.9590403438, alpha: 1)), radius: 6, x: -10, y: -10)
+            
+               
+        }
+    }
+}
+
+struct RingView_Previews: PreviewProvider {
+    static var previews: some View {
+        RingView(show: .constant(true))
+    }
+}
